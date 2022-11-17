d10d382cd2d98040055859570a7ef400df0a0b96
stufromnorthvan <stumitchell@irsss.ca>
2022-11-16 17:41:20 -0800

    add about page and adjust navbar and footer to have links

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
 
   root to: 'products#index'
 
+  resources :about, only: [:index]
+
   resources :products, only: [:index, :show]
   resources :categories, only: [:show]
 
@@ -15,6 +17,7 @@ Rails.application.routes.draw do
   namespace :admin do
     root to: 'dashboard#show'
     resources :products, except: [:edit, :update, :show]
+    resources :about, only: [:index]
   end
 
   # The priority is based upon order of creation: first created -> highest priority.
